01b3665243c1d4f8e2b6e7d4160d8f1424a08fb2
Jeongmin Do <silverstar456@yonsei.ac.kr>
2021-11-30 10:47:02 +0000

    mysql 실행 명령어 분리

--- server-settings.sh
+++ server-settings.sh
@@ -16,13 +16,3 @@ sudo usermod -aG docker $USER # 현재 접속중인 사용자에게 권한주기
 # install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.0-rc2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-# run docker mysql
-docker run -d -it --rm \
-    --name mysql-db \
-    -p 3306:3306 \
-    -e MYSQL_ROOT_PASSWORD=admin123! \
-    -v /home/ubuntu/mysql/data:/var/lib/mysql \
-    -e MYSQL_PASSWORD=admin123! mysql:5.7 \
-    --character-set-server=utf8mb4 \
-    --collation-server=utf8mb4_unicode_ci
