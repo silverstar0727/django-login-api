7c7576c644650d38f3172962127bf23153397180
JeongMin Do <49096513+silverstar0727@users.noreply.github.com>
2021-12-15 16:45:17 +0900

    init

--- docker-server/login/urls.py
+++ docker-server/login/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url('regist_user', views.RegistUser.as_view(), name='regist_user'),
-    url('app_login', views.AppLogin.as_view(), name='app_login'),
+    path('regist_user', views.RegistUser.as_view(), name='regist_user'),
+    path('app_login', views.AppLogin.as_view(), name='app_login'),
 ]
 
